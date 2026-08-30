15bf41c56b58185128961531b4a32e8aa3cbee7a
Faruk <farukw2020@gmail.com>
2026-08-30 11:46:42 +0600

    Another new line my code  Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int number;
+        System.out.println("please enter the number :");
+        Scanner r = new Scanner(System.in);
+        number=r.nextInt();
+        for(int i=0; i<number; i++){
+            System.out.println(i);
+        }
     }
 }
